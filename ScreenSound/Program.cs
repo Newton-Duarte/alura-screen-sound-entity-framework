29dcb1fecc7a7e3cf0de228b8dfc888321705047
Newton Duarte Cavalcanti <newton_@hotmail.com>
2024-02-07 11:06:51 -0300

    feat: add update method to artist dal class

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -5,14 +5,18 @@ using ScreenSound.Modelos;
 try
 {
     var artistaDAL = new ArtistaDAL();
-    artistaDAL.Adicionar(new Artista("Michel Teló", "Michel Teló (Medianeira, 21 de janeiro de 1981) é um cantor e compositor brasileiro de música sertaneja.")
+    //artistaDAL.Adicionar(new Artista("Michel Teló", "Michel Teló (Medianeira, 21 de janeiro de 1981) é um cantor e compositor brasileiro de música sertaneja.")
+    //{
+    //    FotoPerfil = "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //});
+    artistaDAL.Atualizar(new Artista("Michel Teló Da Silva", "Bio atualizada")
     {
-        FotoPerfil = "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        Id = 3,
     });
 
     var lista = artistaDAL.Listar();
 
-    foreach(var item in lista)
+    foreach (var item in lista)
     {
         Console.WriteLine(item);
     }
@@ -70,7 +74,7 @@ void ExibirOpcoesDoMenu()
         Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
         menuASerExibido.Executar(artistasRegistrados);
         if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
-    } 
+    }
     else
     {
         Console.WriteLine("Opção inválida");
